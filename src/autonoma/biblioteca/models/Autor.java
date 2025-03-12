4d9eb5bfb7114e4ea3cfbcd92e09c491cd80403e
TheMonsterOfPy <sant987ago@gmail.com>
2025-03-12 17:48:03 -0500

    Se añaden get y set de Autor

--- src/autonoma/biblioteca/models/Autor.java
+++ src/autonoma/biblioteca/models/Autor.java
@@ -10,4 +10,28 @@ package autonoma.biblioteca.models;
  */
 public class Autor {
     
+    private String editorial;
+    private String profesion;
+
+    public Autor(String editorial, String profesion) {
+        this.editorial = editorial;
+        this.profesion = profesion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+    
 }
