f35b1ac8698ea8d106b4e2ca59b9e26a0e2250a0
María Paz Puerta Acevedo <mariapazpuerta@gmail.com>
2025-03-12 17:15:10 -0500

    Se agrega la clase persona

--- src/autonoma/biblioteca/models/Persona.java
+++ src/autonoma/biblioteca/models/Persona.java
@@ -7,7 +7,58 @@ package autonoma.biblioteca.models;
 /**
  *
  * @author Maria Paz Puerta
+ * @since 20250312
+ * @version 1.0.0
  */
-public class Persona {
+public abstract class Persona {
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //// Atributos
+    
+    /**
+     * Nombre de la persona
+     */
+    private String nombre;
+    
+    /**
+     * Documento de identidad de la persona
+     */
+    private String documentoIdentidad;
+    
+    /**
+     * Email de la persona
+     */
+    private String correoElectronico;
+
+    public Persona(String nombre, String documentoIdentidad, String correoElectronico) {
+        this.nombre = nombre;
+        this.documentoIdentidad = documentoIdentidad;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
     
 }
