b1bf7d3edc8a922293bf9fee3b7437a5a53eea8e
Martin Taborda Aristizabal <martintabordaaristizabal@gmail.com>
2025-03-12 19:09:36 -0500

    Se agrega la clase Biblioteca

--- src/autonoma/biblioteca/models/Biblioteca.java
+++ src/autonoma/biblioteca/models/Biblioteca.java
@@ -11,13 +11,73 @@ import java.util.ArrayList;
  * @author Maria Paz Puerta
  */
 public class Biblioteca {
-    ArrayList<Libro> libro;
+    ArrayList<Libro> libros;
     
     public Biblioteca() {
+        this.libros=new ArrayList<>();
     }
-    
-    
-    
+    public String mostrarLibros() {
+        String resultado = "";
+        for (int i = 0; i < libros.size(); i++) {
+            resultado = resultado + "ID: " + libros.get(i).getId() + ", Título: " + libros.get(i).getTitulo() + "\n";
+        }
+        return resultado;
+    }
+
+    public boolean agregarLibro(Libro libro) {
+        return libros.add(libro);
+    }
+
+    public Libro buscarLibro(long id) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getId() == id) {
+                return libros.get(i);
+            }
+        }
+        return null;
+    }
+
+    public boolean actualizarLibro(long id, Libro nuevoLibro) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getId() == id) {
+                libros.set(i, nuevoLibro);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean eliminarLibro(long id) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getId() == id) {
+                libros.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Libro> obtenerLibrosAlfabeticamente() {
+        ArrayList<Libro> ordenados = new ArrayList<>();
+        for (int i = 0; i < libros.size(); i++) {
+            ordenados.add(libros.get(i));
+        }
+        for (int i = 0; i < ordenados.size() - 1; i++) {
+            for (int j = i + 1; j < ordenados.size(); j++) {
+                if (ordenados.get(i).getTitulo().compareTo(ordenados.get(j).getTitulo()) > 0) {
+                    Libro temp = ordenados.get(i);
+                    ordenados.set(i, ordenados.get(j));
+                    ordenados.set(j, temp);
+                }
+            }
+        }
+        return ordenados;
+    }
 }
+    
+    
+    
+    
+
 
 
