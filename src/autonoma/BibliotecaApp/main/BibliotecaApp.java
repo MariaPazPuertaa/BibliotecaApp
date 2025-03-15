f0bf628fc4bf725b90a1be34eed4e09088ee76dc
Martin Taborda Aristizabal <martintabordaaristizabal@gmail.com>
2025-03-15 13:09:06 -0500

    se agrego varios packetes nuevos

---
+++ src/autonoma/BibliotecaApp/main/BibliotecaApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.BibliotecaApp.main;
+
+import autonoma.biblioteca.models.Biblioteca;
+
+/**
+ *
+ * @author marti
+ */
+public class BibliotecaApp {
+    
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+        
+        //VentanaPrincipal ventana = new VentanaPrincipal(cena);
+        //ventana.setVisible(true);
+    }
+}
